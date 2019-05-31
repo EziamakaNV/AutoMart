53e428d833c5cdeb4da5b01de733a90e6f6caf6c
Nnaemeka Eziamaka <eziamakanv@gmail.com>
2019-05-31 23:49:04 +0100

    feature(upload):add cloudinary to upload pictures - Create cloudinary account - Configure cloudinary on the server - Update the create order API [Finishes #166380077]

--- dist/controllers/Car.js
+++ dist/controllers/Car.js
@@ -9,6 +9,10 @@ require("core-js/modules/es6.symbol");
 
 require("core-js/modules/web.dom.iterable");
 
+var _cloudinary = _interopRequireDefault(require("cloudinary"));
+
+var _fs = _interopRequireDefault(require("fs"));
+
 var _Validation = _interopRequireDefault(require("../validations/Validation"));
 
 var _Car = _interopRequireDefault(require("../models/Car"));
@@ -23,47 +27,113 @@ function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { va
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
+// Cloudinary
+require('dotenv').config();
+
+const cloudinary = _cloudinary.default.v2;
+
 class CarController {
   static createCar(req, res) {
-    const _req$body = req.body,
-          state = _req$body.state,
-          status = _req$body.status,
-          price = _req$body.price,
-          manufacturer = _req$body.manufacturer,
-          model = _req$body.model,
-          bodyType = _req$body.bodyType;
-    const validationObject = {
-      state,
-      status,
-      price,
-      manufacturer,
-      model,
-      bodyType
-    };
+    // If the car image is present
+    if (req.files) {
+      // The image is properly added to the form
+      if (req.files.carImage) {
+        const _req$body = req.body,
+              state = _req$body.state,
+              status = _req$body.status,
+              price = _req$body.price,
+              manufacturer = _req$body.manufacturer,
+              model = _req$body.model,
+              bodyType = _req$body.bodyType;
+        const validationObject = {
+          state,
+          status,
+          price,
+          manufacturer,
+          model,
+          bodyType
+        };
 
-    const _Validation$newCarVal = _Validation.default.newCarValidation(validationObject),
-          error = _Validation$newCarVal.error;
+        const _Validation$newCarVal = _Validation.default.newCarValidation(validationObject),
+              error = _Validation$newCarVal.error;
 
-    if (error) {
-      res.status(400).json({
-        status: 400,
-        error: "Issue with parameters supplied. Problem ".concat(error),
-        success: false
-      });
+        if (error) {
+          res.status(400).json({
+            status: 400,
+            error: "Issue with parameters supplied. Problem ".concat(error),
+            success: false
+          });
+        } else {
+          // Upload to cloudinary
+          const imageFileName = req.files.carImage.path;
+          cloudinary.uploader.upload(imageFileName, {
+            resource_type: 'auto'
+          }, (err, file) => {
+            if (err) {
+              (0, _Response.default)(res, 500, err);
+            } else {
+              const newCarDetails = _objectSpread({}, validationObject, {
+                ownerId: req.user.id,
+                ownerEmail: req.user.email,
+                imageUrl: file.url
+              }); // Create new car
+
+
+              const createdCar = _Car.default.createCar(newCarDetails);
+
+              (0, _Response.default)(res, 201, createdCar); // Delete temporary image file
+
+              _fs.default.unlink(imageFileName, er => {
+                if (er) throw err;
+                console.log('Image File Deleted');
+              });
+            }
+          });
+        }
+      } else {
+        (0, _Response.default)(res, 400, 'Image expected to be named carImage');
+      }
     } else {
-      const newCarDetails = _objectSpread({}, validationObject, {
-        ownerId: req.user.id,
-        ownerEmail: req.user.email
-      }); // Create new car
+      const _req$body2 = req.body,
+            state = _req$body2.state,
+            status = _req$body2.status,
+            price = _req$body2.price,
+            manufacturer = _req$body2.manufacturer,
+            model = _req$body2.model,
+            bodyType = _req$body2.bodyType;
+      const validationObject = {
+        state,
+        status,
+        price,
+        manufacturer,
+        model,
+        bodyType
+      };
+
+      const _Validation$newCarVal2 = _Validation.default.newCarValidation(validationObject),
+            error = _Validation$newCarVal2.error;
+
+      if (error) {
+        res.status(400).json({
+          status: 400,
+          error: "Issue with parameters supplied. Problem ".concat(error),
+          success: false
+        });
+      } else {
+        const newCarDetails = _objectSpread({}, validationObject, {
+          ownerId: req.user.id,
+          ownerEmail: req.user.email
+        }); // Create new car
 
 
-      const createdCar = _Car.default.createCar(newCarDetails);
+        const createdCar = _Car.default.createCar(newCarDetails);
 
-      res.status(201).json({
-        status: 201,
-        data: createdCar,
-        sucess: true
-      });
+        res.status(201).json({
+          status: 201,
+          data: createdCar,
+          sucess: true
+        });
+      }
     }
   }
 
--- src/routes/car.js
+++ src/routes/car.js
@@ -1,12 +1,18 @@
+/* eslint-disable linebreak-style */
 import express from 'express';
 
 import Authentication from '../middleware/Authentication';
 
 import CarController from '../controllers/Car';
 
+// Multipart middleware to parse file uploads
+const multipart = require('connect-multiparty');
+
+const multipartMiddleware = multipart({ uploadDir: './tempImages' }); // Folder to store temp image files n.b delete later
+
 const router = express.Router();
 
-router.post('/', Authentication.verifyToken, CarController.createCar);
+router.post('/', Authentication.verifyToken, multipartMiddleware, CarController.createCar);
 
 router.patch('/:carId/status', Authentication.verifyToken, CarController.updateStatus);
 
